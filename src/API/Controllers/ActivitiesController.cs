0036f79290d3d970ed9fe08a11d9dc43b7a902dd
yurii shchehliuk <shchehliuk124@gmail.com>
2022-11-09 18:54:18 +0100

    repo init

---
+++ src/API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public ActivitiesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity?>> GetActivity(Guid id)
+        {
+            return await _context.Activities.SingleOrDefaultAsync(c => c.Id == id);
+        }
+
+    }
+}
